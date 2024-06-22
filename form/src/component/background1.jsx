47246b73b3e2580a60ad406afeed946d7a4bded7
Vikrant Rana <vikrantrana817@gmail.com>
2024-06-22 16:56:03 +0530

    css fix

--- form/src/component/background1.jsx
+++ form/src/component/background1.jsx
@@ -5,7 +5,7 @@ export default function FirstBg({children}){
     const ref = useRef(null);
 
     return (
-        <div ref={ref} className="w-full h-full bg-red-50 fixed flex items-center justify-center">
+        <div ref={ref} className="w-full h-full bg-black fixed flex items-center justify-center">
                 {children}
             <div className="noise">
             </div>
